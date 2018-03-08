466377f22beb9b742db32eb5ec03532a1c8f5212
Joshua Weinstein <jweinstein@splunk.com>
2018-03-07 16:16:28 -0800

    Initial Commit

---
+++ list-lang-prototype/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  list-lang-prototype
+//
+//  Created by Joshua Weinstein on 3/7/18.
+//  Copyright © 2018 Joshua Weinstein. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
