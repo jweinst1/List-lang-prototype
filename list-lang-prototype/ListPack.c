84dcc8aea156db9ca9c46d72bf1406e9f46b9d79
Joshua Weinstein <jweinstein@splunk.com>
2018-03-08 12:02:04 -0800

    list packing support for new

--- list-lang-prototype/ListPack.c
+++ list-lang-prototype/ListPack.c
@@ -13,3 +13,71 @@ void ListPack_bool(List* lst, int b)
     lst->data[0] = ListType_Bool;
     lst->data[1] = b;
 }
+
+int ListPack_fmt_len(const char* fmt)
+{
+    int total = 0;
+    while(*fmt)
+    {
+        switch(*fmt)
+        {
+            case 'b':
+                total += 2;
+                fmt++;
+                break;
+            default:
+                fprintf(stderr, "Pack Error: Unknown specifier '%c', exiting.\n", *fmt);
+                exit(2);
+        }
+    }
+    return total;
+}
+
+void ListPack_fmt(List* lst, const char* fmt, ...)
+{
+    va_list packArgs;
+    va_start(packArgs, fmt);
+    char* writer = (char*)lst->data;
+    while(*fmt)
+    {
+        switch(*fmt)
+        {
+            case 'b': // Packs a bool into the list.
+                *writer++ = ListType_Bool;
+                *writer++ = va_arg(packArgs, int);
+                fmt++;
+                break;
+            default:
+                fprintf(stderr, "Pack Error: Unknown specifier '%c', exiting.\n", *fmt);
+                exit(2);
+        }
+    }
+    va_end(packArgs);
+    return;
+}
+
+List* ListPack_new(const char* fmt, ...)
+{
+    int sized = ListPack_fmt_len(fmt);
+    List* newlist = malloc(sizeof(List) + sized);
+    newlist->size = sized;
+    va_list packArgs;
+    va_start(packArgs, fmt);
+    char* writer = (char*)newlist->data;
+    while(*fmt)
+    {
+        switch(*fmt)
+        {
+            case 'b': // Packs a bool into the list.
+                *writer++ = ListType_Bool;
+                *writer++ = va_arg(packArgs, int);
+                fmt++;
+                break;
+            default:
+                fprintf(stderr, "Pack Error: Unknown specifier '%c', exiting.\n", *fmt);
+                exit(2);
+        }
+    }
+    va_end(packArgs);
+    return newlist;
+}
--- list-lang-prototype/ListPack.h
+++ list-lang-prototype/ListPack.h
@@ -12,9 +12,20 @@
 // header responsible for packing types or values into lists
 
 #include <stdio.h>
+#include <stdarg.h>
 #include "List.h"
 
 // packs a boolean into a list.
 void ListPack_bool(List* lst, int b);
+
+// calculates and returns the size required for some fmt list specifier
+int ListPack_fmt_len(const char* fmt);
+
+// Packs a list using variadic arguments and a format string.
+void ListPack_fmt(List* lst, const char* fmt, ...);
+
+// Creates a new list from some format string specification.
+List* ListPack_new(const char* fmt, ...);
+
 
 #endif /* ListPack_h */
